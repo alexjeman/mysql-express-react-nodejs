50bde470d83b4f6ba5e3ce959d3c78e58dba8515
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-09 04:21:02 +0200

    Post project route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require("express-validator");
 // @access Public
 router.get("/", async (req, res) => {
   try {
-    let sql = `SELECT user, project_name, project_description, project_link FROM projects`;
+    let sql = `SELECT user, project_name, project_description, project_link, project_img FROM projects`;
     connection.query(sql, async (err, user) => {
       if (err) throw err;
       res.json(user);
@@ -23,9 +23,46 @@ router.get("/", async (req, res) => {
 // @route POST api/projects
 // @desc Add new project
 // @access Private
-router.post("/", (req, res) => {
-  res.send("Add project");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("project_name", "Project name is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      project_name,
+      project_description,
+      project_link,
+      project_img
+    } = req.body;
+    const project = {
+      user: req.user.email,
+      project_name,
+      project_description,
+      project_link,
+      project_img
+    };
+    try {
+      let sqlInsert = `INSERT INTO projects (user, project_name, project_description, project_link, project_img) VALUES ("${project.user}", "${project.project_name}", "${project.project_description}", "${project.project_link}", "${project.project_img}")`;
+      await connection.query(sqlInsert, (err, results) => {
+        if (err) throw err;
+        res.json(project);
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route PUT api/projects/:id
 // @desc Update project
