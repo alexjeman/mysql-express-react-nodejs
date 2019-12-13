06f743b7e2e11b4414e0c4cfc0b44c8614c12759
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-13 03:49:44 +0200

    User login and user logout

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import NotificationContext from "../../context/notification/notificationContext";
+import AuthContext from "../../context/auth/authContext";
 
-const Login = () => {
+const Login = props => {
+  const notificationContext = useContext(NotificationContext);
+  const authContext = useContext(AuthContext);
+
+  const { setNotification } = notificationContext;
+
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/admin");
+    }
+
+    if (error === "Invalid credentials" || error === "Invalid password") {
+      setNotification(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: "",
     password: ""
   });
 
-  const { email, password} = user;
+  const { email, password } = user;
 
   const onChange = e => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -14,7 +35,14 @@ const Login = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Login Submit");
+    if (email === "" || password === "") {
+      setNotification("Please fill in all fields");
+    } else {
+      login({
+        email,
+        password
+      })
+    }
   };
 
   return (
@@ -24,7 +52,13 @@ const Login = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
@@ -33,6 +67,7 @@ const Login = () => {
             name='password'
             value={password}
             onChange={onChange}
+            required
           />
         </div>
         <input type='submit' value='Login' className='btn' />
