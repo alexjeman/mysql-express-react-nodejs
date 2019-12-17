588232a9721ab9e261b3dc54161505600e33aab2
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-18 01:31:47 +0200

    Add missing pages, various small fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  apps : [
-      {
-        name: "server",
-        script: "./server.js",
-        watch: true,
-        env: {
-            "PORT": 5000,
-            "NODE_ENV": "development"
-        },
-        env_production: {
-            "PORT": 80,
-            "NODE_ENV": "production",
-        }
+  apps: [
+    {
+      name: "server",
+      script: "./server.js",
+      watch: true,
+      env: {
+        PORT: 5000,
+        NODE_ENV: "development"
+      },
+      env_production: {
+        PORT: 80,
+        NODE_ENV: "production"
       }
+    }
   ]
-}
+};
