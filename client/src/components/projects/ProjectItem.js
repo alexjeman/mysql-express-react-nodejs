7e7e55cff108b3a38c64954c783824d6736f25ff
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-18 01:30:17 +0200

    Temporarily disable project item rotate effect

--- client/src/components/projects/ProjectItem.js
+++ client/src/components/projects/ProjectItem.js
@@ -9,20 +9,25 @@ const ProjectItem = ({ project }) => {
     project_link,
     project_img
   } = project;
+
   return (
     <div className='project-card'>
       <div className='card'>
-        <div className='card-side card-side-front'>
-          <h3 className='project-name'>{project_name}</h3>
-          <img src={project_img} alt='' />
-        </div>
-        <div className='card-side card-side-back'>
+        <a href={"#popup" + id}>
+          <div className='card-side card-side-front'>
+            <h3 className='project-name'>{project_name}</h3>
+            <img src={project_img} alt='' />
+          </div>
+        </a>
+        {/* <div className='card-side card-side-back'>
           <p>{project_description}</p>
           <a href={"#popup" + id}>View Description</a>
-        </div>
+        </div> */}
       </div>
       <div className='popup' id={"popup" + id}>
-        <a href='#project-page' className='popup-close-main'>{" "}</a>
+        <a href='#project-page' className='popup-close-main'>
+          {" "}
+        </a>
         <div className='popup-content'>
           <div className='popup-left'>
             <img src={project_img} alt='' className='popup-img' />
@@ -30,12 +35,20 @@ const ProjectItem = ({ project }) => {
           </div>
           <div className='popup-right'>
             <a href='#project-page' className='popup-close'>
-              <span className="iconify" data-icon="ion:close-sharp" data-inline="false"></span>
+              <span
+                className='iconify'
+                data-icon='ion:close-sharp'
+                data-inline='false'
+              ></span>
             </a>
             <h2 className='project-name'>{project_name}</h2>
             <p className='popup-text'>{project_description}</p>
-            <a href={project_sample} target="_blank" rel="noopener noreferrer">Code Sample</a>
-            <a href={project_link} target="_blank" rel="noopener noreferrer">View Live</a>
+            <a href={project_sample} target='_blank' rel='noopener noreferrer'>
+              Code Sample
+            </a>
+            <a href={project_link} target='_blank' rel='noopener noreferrer'>
+              View Live
+            </a>
           </div>
         </div>
       </div>
